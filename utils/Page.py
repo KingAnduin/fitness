2c518acbc3a3f072fe2021609783a7bded5a4ba7
KingAnduin <2864595564@qq.com>
2020-04-22 16:13:40 +0800

    分页

--- utils/Page.py
+++ utils/Page.py
@@ -3,7 +3,7 @@ from rest_framework.pagination import PageNumberPagination
 
 class PageSetting(PageNumberPagination):
     # 默认每页显示的数据条数
-    page_size = 3
+    page_size = 5
     # 获取url参数中设置的每页显示数据条数
     page_size_query_param = 'size'
     # 获取url中传入的页码key
